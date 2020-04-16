e723fdb32cdd7a3eb918e408b7b5c4f9321b7561
u5ergen <u5ergen@yandex.ru>
2020-04-16 21:57:26 +0300

    4

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ app.config.from_object(Config)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World! - 5'
+    return 'Hello, World! - 666'
 
 
 @app.route('/update_server', methods=['POST', 'GET'])
