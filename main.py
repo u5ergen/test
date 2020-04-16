71439d06e91c01e26a71da81bcb1a829d5e207a8
u5ergen <u5ergen@yandex.ru>
2020-04-16 20:57:10 +0300

    4

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ from config import Config
 import git
 
 
-# app = Flask(__name__,)
-# app.config.from_object(Config)
+app = Flask(__name__,)
+app.config.from_object(Config)
 
 
 @app.route('/')
@@ -15,16 +15,16 @@ def hello_world():
     return 'Hello, World! - 1'
 
 
-# @app.route('/update_server', methods=['POST', 'GET'])
-# def webhook():
-# 	print('\n\n\n\nwebhook\n\n\n\n')
-# 	if request.method == 'POST':
-# 		repo = git.Repo('https://github.com/u5ergen/test.git')
-# 		origin = repo.remotes.origin
-# 		origin.pull()
-# 		return 'Updated PythonAnywhere successfully', 200
-# 	else:
-# 		return 'Wrong event type', 400 
+@app.route('/update_server', methods=['POST', 'GET'])
+def webhook():
+	print('\n\n\n\nwebhook\n\n\n\n')
+	if request.method == 'POST':
+		repo = git.Repo('https://github.com/u5ergen/test.git')
+		origin = repo.remotes.origin
+		origin.pull()
+		return 'Updated PythonAnywhere successfully', 200
+	else:
+		return 'Wrong event type', 400 
 
 
 if __name__ == '__main__':
