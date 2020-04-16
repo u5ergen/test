c59a197fc4f62d03d004cdb429eaf6babd1031dd
u5ergen <u5ergen@yandex.ru>
2020-04-16 17:24:30 +0300

    3

--- main.py
+++ main.py
@@ -9,12 +9,12 @@ def hello_world():
     return 'Hello, World! - 1'
 
 
-@app.route('/update_server', methods=['POST'])
-def webhook():
-	if request.method == 'POST':
-		repo = git.Repo('https://github.com/u5ergen/test.git')
-		origin = repo.remotes.origin
-		origin.pull()
-		return 'Updated PythonAnywhere successfully', 200
-	else:
-		return 'Wrong event type', 400 
+# @app.route('/update_server', methods=['POST'])
+# def webhook():
+# 	if request.method == 'POST':
+# 		repo = git.Repo('https://github.com/u5ergen/test.git')
+# 		origin = repo.remotes.origin
+# 		origin.pull()
+# 		return 'Updated PythonAnywhere successfully', 200
+# 	else:
+# 		return 'Wrong event type', 400 
