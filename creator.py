2d24cecd6b56d977e9e2c22b8b10ebf17c279976
u5ergen <u5ergen@yandex.ru>
2020-05-02 10:46:16 +0300

    BASE_DIR

--- creator.py
+++ creator.py
@@ -76,7 +76,7 @@ def make_deck_info_dict(deckstring, lang='enUS', send_existing_files = False):
 
 	deck_info['decklist'] = decklist
 
-	tmp.remove_old_temp_files(buffer=30, temp_dir=TEMP_DIR)
+	tmp.remove_old_temp_files(buffer=30)
 
 
 
