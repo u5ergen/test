e1b25a2a21bb155d4888715a470eb8f03d82b961
u5ergen <u5ergen@yandex.ru>
2020-05-03 22:26:02 +0300

    script.js

--- main.py
+++ main.py
@@ -33,48 +33,51 @@ class SendCodeString(FlaskForm):
 def index():
     # form = SendCodeString()
 
-    if request.method == 'POST':
-        deckstring = request.form.get('deck_string_form')
-        lang = request.form.get('options_LANG')
-        print(deckstring)
-        print(lang)
-        # decklist = create(deckstring)
+    # if request.method == 'POST':
+    #     deckstring = request.form.get('deck_string_form_input')
+    #     lang = request.form.get('options_LANG')
+    #     print(deckstring)
+    #     print(lang)
+    #     # decklist = create(deckstring)
 
-        # images_filenames = []
-        # bg_top = 'static/images/bg_top_MAGE.png'
-        # # for i in range(2):
-        # decklen = len(decklist)
-        # for i in range(len(decklist)):
-        #     url = f"https://art.hearthstonejson.com/v1/render/latest/enUS/256x/{decklist[i][0]['id']}.png";
-        #     if decklist[i][0]['cardClass'] and decklist[i][0]['cardClass'] != 'NEUTRAL':
-        #         bg_top = 'static/images/bg_top_' + decklist[i][0]['cardClass'] + '.png?1'
-        #     # url = 'https://hsto.org/r/w32/webt/5a/de/0e/5ade0efe6f5d5276653463.png'
-        #     # url = 'https://via.placeholder.com/256x387'
+    #     # images_filenames = []
+    #     # bg_top = 'static/images/bg_top_MAGE.png'
+    #     # # for i in range(2):
+    #     # decklen = len(decklist)
+    #     # for i in range(len(decklist)):
+    #     #     url = f"https://art.hearthstonejson.com/v1/render/latest/enUS/256x/{decklist[i][0]['id']}.png";
+    #     #     if decklist[i][0]['cardClass'] and decklist[i][0]['cardClass'] != 'NEUTRAL':
+    #     #         bg_top = 'static/images/bg_top_' + decklist[i][0]['cardClass'] + '.png?1'
+    #     #     # url = 'https://hsto.org/r/w32/webt/5a/de/0e/5ade0efe6f5d5276653463.png'
+    #     #     # url = 'https://via.placeholder.com/256x387'
             
-        #     foldername, filename = tmp.make_temp_file_from_url(url, temp_dir=TEMP_DIR) # Can return None
-        #     print(f'{i} from {decklen}:', filename)
-        #     images_filenames.append('static/images/temp/' + filename)
-        # images_filenames.extend([bg_top, 'static/images/x2-42.png'])
+    #     #     foldername, filename = tmp.make_temp_file_from_url(url, temp_dir=TEMP_DIR) # Can return None
+    #     #     print(f'{i} from {decklen}:', filename)
+    #     #     images_filenames.append('static/images/temp/' + filename)
+    #     # images_filenames.extend([bg_top, 'static/images/x2-42.png'])
 
-        # tmp.remove_old_temp_files(buffer=30, temp_dir=TEMP_DIR)
+    #     # tmp.remove_old_temp_files(buffer=30, temp_dir=TEMP_DIR)
+    #     try:
+    #     	deck_info = make_deck_info_dict(deckstring, lang=lang) #, send_existing_files = True)
+    #     except:
+    #     	deck_info = {'decklist': 0}
+    #     # deck_info = {}
+    # else:
+    #     # decklist = create('AAEBAf0EBpwCuAj3DdDBArnRAsW4AwyKAcAByQOrBMsE5gT4B5jEAtrFArT8Ap+bA/+dAwA=')
+    #     # decklist = 0
+    #     images_filenames = ['static/images/temp/' + filename for filename in os.listdir(TEMP_DIR)]
+    #     images_filenames.extend(['static/images/bg_top_ROGUE.png?1', 'static/images/x2-42.png'])
 
-        deck_info = make_deck_info_dict(deckstring, lang=lang) #, send_existing_files = True)
-        # deck_info = {}
-    else:
-        # decklist = create('AAEBAf0EBpwCuAj3DdDBArnRAsW4AwyKAcAByQOrBMsE5gT4B5jEAtrFArT8Ap+bA/+dAwA=')
-        # decklist = 0
-        images_filenames = ['static/images/temp/' + filename for filename in os.listdir(TEMP_DIR)]
-        images_filenames.extend(['static/images/bg_top_ROGUE.png?1', 'static/images/x2-42.png'])
-
-        deckstring = 'AAECAZ/HAgbTCuubA5+pA+O0A8i+A8jAAwzcAckGigfRpQOZqQPXrAParAPyrAOqrwPNrwOTugPpvgMA'
-        # deck_info = make_deck_info_dict(deckstring, send_existing_files = True)
-        deck_info = {'decklist': 0}
+    #     deckstring = 'AAECAZ/HAgbTCuubA5+pA+O0A8i+A8jAAwzcAckGigfRpQOZqQPXrAParAPyrAOqrwPNrwOTugPpvgMA'
+    #     # deck_info = make_deck_info_dict(deckstring, send_existing_files = True)
+    #     deck_info = {'decklist': 0}
+    #     deck_info = make_deck_info_dict(deckstring, lang='enUS', send_existing_files = True)
 
 
-    decklist = {'color':'#909000'}
-    images_filenames = {'color':'#909000'}
+    # decklist = {'color':'#909000'}
+    # images_filenames = {'color':'#909000'}
 
-    return render_template('index.html', decklist=decklist, images_filenames = images_filenames, deck_info = deck_info)
+    return render_template('index.html') #, decklist=decklist, images_filenames = images_filenames, deck_info = deck_info)
 
 
 # refresh css
@@ -92,10 +95,40 @@ def dated_url_for(endpoint, **values):
     return url_for(endpoint, **values)
 
 
+@app.route('/test_flex')
+def test_flex():
+	return render_template('test_flex.html')
+
+
 @app.route('/get_len', methods=['GET', 'POST'])
 def get_len():
-	name = request.form.get('deck_string_form')
+	name = request.form.get('deck_string_form_input')
 	return json.dumps({'len': len(name)})
+
+
+@app.route('/get_deck_info', methods=['GET', 'POST'])
+def get_deck_info():
+	print('/get_deck_info')
+
+	if request.method == 'POST':
+		deckstring = request.form.get('deck_string_form_input')
+		lang = request.form.get('options_LANG')
+		print(deckstring)
+		print(lang)
+		try:
+			deck_info = make_deck_info_dict(deckstring, lang=lang, send_existing_files = True)
+		except:
+			deck_info = {'decklist': 0}
+	else:
+		images_filenames = ['static/images/temp/' + filename for filename in os.listdir(TEMP_DIR)]
+		images_filenames.extend(['static/images/bg_top_ROGUE.png?1', 'static/images/x2-42.png'])
+
+		deckstring = 'AAECAZ/HAgbTCuubA5+pA+O0A8i+A8jAAwzcAckGigfRpQOZqQPXrAParAPyrAOqrwPNrwOTugPpvgMA'
+		deck_info = {'decklist': 0}
+		deck_info = make_deck_info_dict(deckstring, lang='enUS', send_existing_files = True)
+
+
+	return json.dumps(deck_info)
 
 
 @app.route('/update_server', methods=['POST', 'GET'])
--- static/js/script.js
+++ static/js/script.js
@@ -1,124 +1,175 @@
-var decklist = {{ decklist|tojson }};
-var bg_top = {{ bg_top|tojson }};
-var images = {{ images_filenames|tojson }};
-var site = "https://art.hearthstonejson.com/v1/render/latest/enUS/256x/{{decklist[0][0]['id']}}.png";
+function get_deck_info() {
+    $.ajax({
+        type: "POST",
+        url: "/get_deck_info",
+        data: $('form').serialize(),
+        type: 'POST',
+        success: function(response) {
+            var deck_info = jQuery.parseJSON(response)
+            // $('#len').html(json.len)
+            // console.log(deck_info);
 
 
-// var images = [];
-var x = [];
-var y = [];
-for (var i = 0; i < decklist.length; i++) {
-    // images[i] = "https://art.hearthstonejson.com/v1/render/latest/enUS/256x/"+decklist[i][0]['id']+".png";
-    x[i] = decklist[i][2][0]
-    y[i] = decklist[i][2][1]
+    if (deck_info['decklist'] != 0) {
+
+        var download_btn = document.getElementById("download_btn");
+        var percentage = 0;
+        function update_download_btn() {
+            percentage = percentage + Math.round(100/deck_info['images_filenames_all'].length)
+            download_btn.innerHTML = '<span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span> Loading... ' + percentage + '%'
+        }
+
+        var loadedImages = {};
+        var promiseArray = deck_info['images_filenames_all'].map(function(image_url){
+            update_download_btn()
+
+            var prom = new Promise(function(resolve,reject){
+                var img = new Image();
+                img.onload = function(){
+                    loadedImages[image_url] = img;
+                    resolve();
+                };
+                img.src = image_url;
+            });
+            return prom;
+        });
+
+        Promise.all(promiseArray).then(drawCanvas);
+        }
+
+    function drawCanvas(){
+        download_btn.innerHTML = 'Download'
+        download_btn.disabled = false;
+
+        var options = {'radio_option_6': 6, 'radio_option_7': 7, 'radio_option_8': 8, 'radio_option_10': 10, }
+        var cols = options[document.querySelector('input[name="options"]:checked').id] //which radio button selected
+
+        var canvas = document.getElementById("canvas");
+        var context = canvas.getContext('2d');
+        context.clearRect(0, 0, 1792, 1200) ///! move into model
+
+        var height_correction = 300 * (cols*0.1); ///? move into model
+
+        canvas.width = deck_info['card_width'] * cols
+        canvas.height = (deck_info['card_height'] * (Math.ceil(deck_info['decklist'].length / cols))) + height_correction;
+
+        context.fillStyle = 'rgba(0,0,0,1)';
+        context.fillRect(0, 0, canvas.width, canvas.height);
+
+        context.drawImage(loadedImages[deck_info['class_bg_top']], 0, 0, canvas.width, canvas.width*560/2600);  ///? move into model
+
+        for (var i = 0; i < deck_info['decklist'].length; i++) {
+            if (i <= cols-1) {
+                position_x = deck_info['card_width']*i;
+                position_y = deck_info['card_height']*0;
+            } else if (i <= cols*2 - 1) {
+                position_x = deck_info['card_width'] * (i-cols);
+                position_y = deck_info['card_height'];
+            } else if (i <= cols*3 - 1) {
+                position_x = deck_info['card_width'] * (i-cols*2);
+                position_y = deck_info['card_height']*2;
+            } else if (i <= cols*4 - 1) {
+                position_x = deck_info['card_width'] * (i-cols*3);
+                position_y = deck_info['card_height']*3;
+            } else if (i <= cols*5 - 1) {
+                position_x = deck_info['card_width'] * (i-cols*4);
+                position_y = deck_info['card_height']*4;
+            }
+
+            if (deck_info['decklist'][i][0]['type'] == 'HERO') {
+                position_x = position_x - 7
+                position_y = position_y - 20
+            }
+
+            position_y = position_y + height_correction
+
+            if (deck_info['decklist'][i][1] == 2) {
+                context.drawImage(loadedImages[deck_info['x2_image']['src']], 
+                    position_x + deck_info['x2_image']['position_correction'][0], 
+                    position_y + deck_info['x2_image']['position_correction'][1]);
+            }
+
+            context.shadowBlur = 20;
+            context.shadowColor = "#000000";
+            context.drawImage(loadedImages[deck_info['images_filenames_cards'][i]], position_x, position_y);
+        }
+
+        // context.shadowBlur = 0;
+        // context.fillStyle = 'rgba(45,45,45,1)';
+        // context.font = "36px Roboto";
+        // context.fillText(deck_info['class_name'] + "'s Deck. Costs: " + deck_info['deck_cost'], 100, 120); ///? move into model
+        // context.fillText("Format: " + deck_info['format'], 100, 160); ///? move into model
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        },
+        error: function(error) {
+            console.log(error);
+        }
+    });
 }
 
-images[images.length] = bg_top //"static/images/bg_top_rogue.jpg"
-images[images.length] = "static/images/x2-42.png"
 
 
-var loadedImages = {};
-var promiseArray = images.map(function(image_url){
-   var prom = new Promise(function(resolve,reject){
-       var img = new Image();
-       img.onload = function(){
-           loadedImages[image_url] = img;
-           resolve();
-       };
-       img.src = image_url;
-   });
-   return prom;
-});
 
-Promise.all(promiseArray).then(imagesRedraw);
 
-function imagesLoaded(){
-    console.log(document.querySelector('input[name="options"]:checked').id) //which radio button selected
+function downloadImage(){
+    var download = document.getElementById("download");
+    var image = document.getElementById("canvas").toDataURL("image/png")
+    .replace("image/png", "image/octet-stream");
+    download.setAttribute("href", image);
 
-   //start canvas work.
-   //when needing to draw image, access the loaded image in loadedImages
-    var canvas = document.getElementById("canvas");
-    var context = canvas.getContext('2d');
-    // context.fillStyle = 'rgba(0,55,0,0.5)';
-    // context.fillRect(0, 0, canvas.width, canvas.height);
-
-    var x2 = images[images.length-1]
-    for (var i = 0; i < decklist.length+1; i++) {
-        context.drawImage(loadedImages[images[i]], x[i], y[i]);
-        if (decklist[i][1] == 2) {
-            context.drawImage(loadedImages[x2], x[i]+98, y[i]+340);
-        }
-    }
-   // context.drawImage(loadedImages[images[0]], 300, 100);
-   console.log(loadedImages[images[0]])
-   console.log(loadedImages[images[images.length - 1]])
 }
 
-function imagesRedraw(){
-
-    var options = {'radio_option_6': 6, 'radio_option_7': 7, 'radio_option_8': 8, 'radio_option_10': 6, }
-    var cols = options[document.querySelector('input[name="options"]:checked').id] //which radio button selected
-    console.log(cols)
-
-    var canvas = document.getElementById("canvas");
-    var context = canvas.getContext('2d');
-    context.clearRect(0, 0, 1792, 1200)
-
-
-    var card_width = 256;
-    var card_height = 388;
-    var card_down = 340;
-    var height_correction = 300 * (cols*0.1);
-
-    canvas.width = card_width * cols
-    canvas.height = (card_height * (Math.ceil(decklist.length / cols))) + height_correction;
-    // console.log(Math.ceil(decklist.length / cols))
-
-    context.fillStyle = 'rgba(0,0,0,1)';
-    // context.fillStyle = 'rgba(33,20,57,1)';
-    context.fillRect(0, 0, canvas.width, canvas.height);
-    //bg_top
-    context.drawImage(loadedImages[images[images.length - 2]], 0, 0, canvas.width, canvas.width*560/2600);
-
-
-    for (var i = 0; i < decklist.length; i++) {
-
-        if (i <= cols-1) {
-            xxx = card_width*i;
-            yyy = card_height*0;
-        } else if (i <= cols*2 - 1) {
-            xxx = card_width * (i-cols);
-            yyy = card_height;
-        } else if (i <= cols*3 - 1) {
-            xxx = card_width * (i-cols*2);
-            yyy = card_height*2;
-        } else if (i <= cols*4 - 1) {
-            xxx = card_width * (i-cols*3);
-            yyy = card_height*3;
-        } else if (i <= cols*5 - 1) {
-            xxx = card_width * (i-cols*4);
-            yyy = card_height*4;
+function get_len() {
+    console.log('get_len START')
+    $.ajax({
+        type: "POST",
+        url: "/get_len",
+        data: $('form').serialize(),
+        type: 'POST',
+        success: function(response) {
+            var json = jQuery.parseJSON(response)
+            // $('#len').html(json.len)
+            console.log(response);
+        },
+        error: function(error) {
+            console.log(error);
         }
-
-        if (decklist[i][0]['type'] == 'HERO') {
-            xxx = xxx - 7
-            yyy = yyy - 23
-        }
-
-        yyy = yyy + height_correction
-
-        if (decklist[i][1] == 2) {
-            context.drawImage(loadedImages[images[images.length - 1]], xxx+98, yyy+340);
-        }
-        context.drawImage(loadedImages[images[i]], xxx, yyy);
-    }
+    });
 }
 
+function checkDeckStringExistence(form_input) {
+    var status = document.getElementById("status_span");
 
-    function download_image(){
-        var download = document.getElementById("download");
-        var image = document.getElementById("canvas").toDataURL("image/png")
-        .replace("image/png", "image/octet-stream");
-        download.setAttribute("href", image);
+    // console.log(form_input.value)
+    var re = /AAE\S*/
+    var deckstring = re.exec(form_input.value)
+    // console.log(deckstring)
 
+    if (deckstring != null) {
+        form_input.value = deckstring[0]
+
+        status.innerHTML = 'Seems like code';
+        status.style.color = '#007700';
+        download.disabled = false;
+    } else {
+        status.innerHTML = 'Code not found';
+        status.style.color = '#aa0000';
+        // status.style.background = '#BFA487';
     }
+    
+}
