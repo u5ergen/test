b213db3c1edf0226c3f9d862dc00829d5befe5ed
u5ergen <u5ergen@yandex.ru>
2020-05-08 17:38:36 +0300

    os.environ

--- config.py
+++ config.py
@@ -6,4 +6,4 @@ class Config:
 	BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 	TEMP_DIR = os.path.join(BASE_DIR, 'static/images/temp/')
 
-	HSSITE_MODE = os.getenv('HSSITE_MODE')
+	HSSITE_MODE = os.environ['HSSITE_MODE']
